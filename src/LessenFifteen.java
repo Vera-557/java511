118cb9d89ff7ec82e7e2c50f5f03d87933b51916
Daiquiri <mestelo@inbox.ru>
2025-11-30 00:19:59 +0300

    урок 15, реализация методов строк 2 часть

---
+++ src/LessenFifteen.java
@@ -0,0 +1,128 @@
+public class LessenFifteen {
+//реализация методов строк 2 часть
+// concat
+// equals
+// equalsIgnoreCase
+// startsWith
+// endsWith
+// substring(int startIndex)
+// substring(int startIndex,int endIndex)
+public static char[] substring(char[] array, int startIndex, int endIndex) {
+    if (startIndex < 0 || endIndex > array.length || startIndex > endIndex) {
+        String end = "Неверные индексы!";
+        return end.toCharArray();
+    }
+    int newSize = endIndex - startIndex;
+    char[] substring = new char[newSize];
+    for (int i = startIndex, j = 0; i < endIndex; i++, j++) {
+        substring[j] = array[i];
+    }
+    return substring;
+}
+
+    public static char[] substring(char[] array, int start) {
+        int newSize = array.length - start;
+        char[] substring = new char[newSize];
+        for (int i = start, j = 0; i < array.length; i++, j++) {
+            substring[j] = array[i];
+        }
+        return substring;
+    }
+
+    public static char[] concat(char[] array, char[] array2) {
+        int stringLength = array.length + array2.length;
+        char[] buf = new char[stringLength];
+        for (int i = 0; i < array.length; i++) {
+            buf[i] = array[i];
+        }
+        for (int i = 0; i < array2.length; i++) {
+            buf[array.length + i] = array2[i];
+        }
+        return buf;
+    }
+
+    static boolean equalsIgnorCase(char[] str1, char[] str2) {
+        if (str1.length != str2.length) {
+            return false;
+        }
+        for (int i = 0; i < str1.length; i++) {
+            char c1 = str1[i];
+            char c2 = str2[i];
+            if (c1 >= 65 && c1 <= 90) {
+                c1 += 32;
+            }
+            if (c2 >= 65 && c2 <= 90) {
+                c2 += 32;
+            }
+            if (c1 != c2) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean equals(char[] str1, char[] str2) {
+        if (str1.length != str2.length) {
+            return false;
+        }
+        for (int i = 0; i < str1.length; i++) {
+
+            if (str1[i] != str2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean startsWith(char[] array, char[] start) {
+        if (start.length > array.length) {
+            return false;
+        }
+        for (int i = 0; i < start.length; i++) {
+            if (array[i] != start[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean endsWith(char[] array, char[] end) {
+        if (end.length > array.length) {
+            return false;
+        }
+        int startPosition = array.length - end.length;
+
+        for (int i = startPosition, j = 0; i < array.length; i++, j++) {
+            if (array[i] != end[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void print(char[] array){
+        for (int i = 0 ; i < array.length; i ++){
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String stringOne = "Чашка чая ";
+        String stringTwo = "горячая!";
+        String stringForEquals = "Чашка Чая ";
+        String stringForSubstringAndOthers = "Ча";
+        char[] array1 = stringOne.toCharArray();
+        char[] array2 = stringTwo.toCharArray();
+        char[] arrayForEquals = stringForEquals.toCharArray();
+        char[] forSubstringAndOthers = stringForSubstringAndOthers.toCharArray();
+        System.out.println("Метод concat: " + concat(array1, array2));
+        System.out.println("Метод equals: (сравним строки " + stringOne + " и "  + stringForEquals + ") : " + equals(array1, arrayForEquals));
+        System.out.println("Метод equalsIgnorCase: : (сравним строки " + stringOne + " и "  + stringForEquals + ") : " + equalsIgnorCase(array1, arrayForEquals));
+        System.out.println("Метод startsWith: (строка " + stringOne + ", начало = " + stringForSubstringAndOthers + ") : " + startsWith(array1, forSubstringAndOthers));
+        System.out.println("Метод endsWith: (строка " + stringOne + ", конец = " + stringForSubstringAndOthers + ") : " + endsWith(array1, forSubstringAndOthers));
+        System.out.println("Метод substring: строка = " + stringOne + ", индекс начала = 5");
+        print(substring(array1, 5));
+        System.out.println("Метод substring: строка = " + stringOne + ", индекс начала = 2, индекс конца = 5");
+        print(substring(array1, 2, 5));
+    }
+}
