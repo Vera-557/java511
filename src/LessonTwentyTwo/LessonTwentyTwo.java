981ccdd60ec526017a8854ca2221efd98f5a2532
Daiquiri <mestelo@inbox.ru>
2025-12-24 23:02:26 +0300

    Урок 21

--- src/LessonTwentyTwo/LessonTwentyTwo.java
+++ src/LessonTwentyTwo/LessonTwentyTwo.java
@@ -1,15 +1,17 @@
 package LessonTwentyTwo;
 
+import java.lang.reflect.Array;
+
 public class LessonTwentyTwo {
     //Выполнить от 1 - 9
 //1    +   public  void addEnd(int num);
 //2    +   public  void print();
-//3    -   public  void addStart(int num);
-//4    -   public  int getFirst();
-//5    -   public  boolean isHasValue(int num);
-//6    -   public  int size();
-//7    -   public  void clear();
-//8    -   public  void removeByIndex(int index);
+//3    +   public  void addStart(int num);
+//4    +   public  int getFirst();
+//5    +   public  boolean isHasValue(int num);
+//6    +   public  int size();
+//7    +   public  void clear();
+//8    +   public  void removeByIndex(int index);
 //9    +   public  void removeByValueFirst(int num);
 
     int[] arr;
@@ -76,6 +78,40 @@ public class LessonTwentyTwo {
             arr = tmp;
         }
     }
-
-
+    public  int getFirst(){
+        if (arr.length == 0){
+            System.out.println("Здравствуйте! Ваш массив пустой, до свидания.");
+        }
+        return arr[0];
+    }
+    public  boolean isHasValue(int num){
+        for (int i = 0; i< arr.length; i ++){
+            if (arr[i] == num){
+                return true;
+            }
+        }
+        return false;
+    }
+    public  int size(){
+        return arr.length;
+    }
+    public  void clear(){
+        arr = null;
+        size = 0;
+    }
+    public  void removeByIndex(int index){
+        if (index < 0 || index >= size) {
+            System.out.println("Упс! Что-то пошло не так. Введен не правильный индекс");
+            return;
+        }
+        int [] temp = new int[arr.length - 1];
+        for (int i=0, j = 0; i < arr.length; i ++){
+            if (i == index) {
+               continue;
+            }
+            temp[j] = arr[i];
+            j++;
+        }
+        arr = temp;
+    }
 }
