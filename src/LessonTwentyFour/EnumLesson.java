53133b458a37476658bf7c60a4fb1a1a7f61ca7f
Daiquiri <mestelo@inbox.ru>
2025-12-28 16:42:48 +0300

    Урок 24 готов

--- src/LessonTwentyFour/EnumLesson.java
+++ src/LessonTwentyFour/EnumLesson.java
@@ -10,13 +10,14 @@ public class EnumLesson {
         System.out.println("У нас выходной? " + DayOfWeek.SATURDAY.dayOfWeekInRussian + " " + DayOfWeek.SATURDAY.isWeekend());
         System.out.println("У нас выходной? " + DayOfWeek.SUNDAY.dayOfWeekInRussian + " " + DayOfWeek.SUNDAY.isWeekend());
         System.out.println("\n\nજ⁀➴જ⁀➴જ⁀➴*ੈ\uD804༘⋆\uD83D\uDCA3\t\t\t\t\tЗадание 2:\n\n");
-        System.out.println(Operation.ADD.apply + " " + Operation.ADD.apply(4, 8.5));
-        System.out.println(Operation.SUBTRACT.apply + " " + Operation.SUBTRACT.apply(4, 8.5));
-        System.out.println(Operation.MULTIPLY.apply + " " + Operation.MULTIPLY.apply(4, 8.5));
-        System.out.println(Operation.DIVIDE.apply + " " + Operation.DIVIDE.apply(4, 8.5));
+        System.out.println(Operation.ADD.option + " " + Operation.ADD.apply(4, 8.5));
+        System.out.println(Operation.SUBTRACT.option + " " + Operation.SUBTRACT.apply(4, 8.5));
+        System.out.println(Operation.MULTIPLY.option + " " + Operation.MULTIPLY.apply(4, 8.5));
+        System.out.println(Operation.DIVIDE.option + " " + Operation.DIVIDE.apply(4, 8.5));
         System.out.println("\n\nજ⁀➴જ⁀➴જ⁀➴*ੈ\uD804༘⋆\uD83D\uDCA3\t\t\t\t\tЗадание 3:\n\n");
         System.out.println("Статус заказа: " + OrderStatus.NEW.status + (OrderStatus.NEW.isCompleted()? " : завершен" : " : не завершен"));
         System.out.println("Статус заказа: " + OrderStatus.PROCESSING.status + (OrderStatus.PROCESSING.isCompleted()? " : завершен" : " : не завершен"));
+        System.out.println("Статус заказа: " + OrderStatus.SHIPPED.status + (OrderStatus.SHIPPED.isCompleted()? " : завершен" : " : не завершен"));
         System.out.println("Статус заказа: " + OrderStatus.DELIVERED.status + (OrderStatus.DELIVERED.isCompleted()? " : завершен" : " : не завершен"));
         System.out.println("Статус заказа: " + OrderStatus.CANCELLED.status + (OrderStatus.CANCELLED.isCompleted()? " : завершен" : " : не завершен"));
         System.out.println("\n\nજ⁀➴જ⁀➴જ⁀➴*ੈ\uD804༘⋆\uD83D\uDCA3\t\t\t\t\tЗадание 4:\n\n");
