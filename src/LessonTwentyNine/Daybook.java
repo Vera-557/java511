e4da1cc6998f15f03e41dea908fa77e1b7c8d8d1
Daiquiri <mestelo@inbox.ru>
2026-02-06 14:05:39 +0300

    Урок 27 готов

--- src/LessonTwentyNine/Daybook.java
+++ src/LessonTwentyNine/Daybook.java
@@ -1,4 +1,9 @@
 package LessonTwentyNine;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 /*
 Задание 3: Дневник событий
 Создайте программу-дневник:
@@ -10,4 +15,27 @@ package LessonTwentyNine;
 Выход
  */
 public class Daybook {
+    Scanner scanner = new Scanner(System.in);
+    File file = new File("C:\\Users\\Public\\Documents\\DayBook.txt");
+    LocalDateTime now = LocalDateTime.now();
+
+    private void add() throws IOException {
+        if (!file.exists()){
+            file.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(file, true);
+        fileWriter.append("\n" + now + "\n" + scanner.nextLine());
+    }
+    private void showAll () throws IOException {
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        bufferedReader.readLine();
+        bufferedReader.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Daybook day = new Daybook();
+        day.add();
+        day.showAll();
+    }
 }
